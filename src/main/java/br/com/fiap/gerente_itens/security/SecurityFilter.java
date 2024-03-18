019c600c5f683c72b66c5663c0bdc34759cdd027
cyph3rk <cyph3rk@gmail.com>
2024-03-17 21:44:56 -0300

    Implementado integralçao jwt

---
+++ src/main/java/br/com/fiap/gerente_itens/security/SecurityFilter.java
@@ -0,0 +1,69 @@
+package br.com.fiap.gerente_itens.security;
+
+import br.com.fiap.gerente_itens.model.Usuario;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.NoSuchElementException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    TokenService tokenService;
+    @Autowired
+    private RestTemplate restTemplate;
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        if(token != null){
+            var login = tokenService.validateToken(token);
+
+            ResponseEntity<String> responseAPI;
+            try {
+                responseAPI = restTemplate.getForEntity(
+                        "http://localhost:8082/users/filter/{login}",
+                        String.class,
+                        login
+                );
+
+                UserDetails user = modelMapper.map(responseAPI.getBody(), Usuario.class);
+
+                var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            } catch (HttpClientErrorException e) {
+                throw new NoSuchElementException();
+            }
+
+
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+}
+
